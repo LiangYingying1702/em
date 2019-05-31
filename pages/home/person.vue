bd7a25a914d95ff1a130e2085ecc00fd325f40e4
LiangYingying1702 <34234963+LiangYingying1702@users.noreply.github.com>
2019-05-31 10:18:42 +0800

    Add files via upload

---
+++ pages/home/person.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		<view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	
+	export default {
+		
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
